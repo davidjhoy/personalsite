7d4bc1ed049ad4c6d0a0ad459e3400c12e419136
david <davidjhoyjr@gmail.com>
2022-07-25 15:21:16 -0400

    working on blog

--- Frontend/frontend/src/components/Blog/Blog.jsx
+++ Frontend/frontend/src/components/Blog/Blog.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 const Blog = () => {
+
+  useEffect(()=>{
+    fetch("https://api.medium.com/v1/users/{{@davidjhoyjr}}/publications")
+    .then((response)=> response.json())
+    .then((data)=>{console.log(data)})
+  },[])
   return (
     <div>Blog</div>
   )
--- Frontend/frontend/src/components/Resume/Resume.jsx
+++ Frontend/frontend/src/components/Resume/Resume.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import { Document, Page, pdfjs } from "react-pdf";
+import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
+import React, {useState} from 'react';
 
-const Resume = () => {
+pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+
+function Resume() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
   return (
-    <>
-      <div>Resume</div>
-      {/* <a href = {Rez} download>download</a> */}
-    </>
-  )
+    <div>
+      <Document file="somefile.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
+  );
 }
 
 export default Resume;
