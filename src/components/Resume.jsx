8de6ed00048e9d79a9f8d147975d07e40899bc60
david <davidjhoyjr@gmail.com>
2022-09-07 12:26:23 -0400

    Revert "newhead"
    This reverts commit 88487dfbcd7c9b2203c11baf2c7b56b171c94e68.

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,9 +1,10 @@
-
+import { Document, Page, pdfjs } from "react-pdf";
+import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
 import React, {useState} from 'react';
 import resume from '../assets/Resume.jpg';
 import "./resume.css"
 
-
+pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 function Resume() {
  
@@ -11,9 +12,7 @@ function Resume() {
 
   return (
     <div className = "resumecontainer">
-      <a href = {resume} download = "David Hoy Resume" target = "_blank">
-      <img src = {resume} id = "resumeimage" download/>
-      </a>
+      <img src = {resume} id = "resumeimage" />
     </div>
   );
 }
